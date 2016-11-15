33994dff993d13e26129c28b7778e2af0ccbc1eb
Amit Asghar <aasghar@loyalty.com>
2016-11-15 17:20:57 -0500

    edit videos

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -6,6 +6,14 @@ app.config(['$routeProvider', function($routeProvider){
 		    templateUrl: 'partials/home.html',
 		    controller: 'HomeCtrl'
 		})
+        .when('/add-video', {
+            templateUrl: 'partials/video-form.html',
+            controller: 'AddVideoCtrl'
+        })
+	    .when('/video/:id', {
+	        templateUrl: 'partials/video-form.html',
+	        controller: 'EditVideoCtrl'
+	    })        
         .otherwise({
             redirectTo: '/'
         });
@@ -17,4 +25,31 @@ app.controller('HomeCtrl', ['$scope', '$resource',
         Videos.query(function(videos){
             $scope.videos = videos;
         });
+    }]);
+
+app.controller('AddVideoCtrl', ['$scope', '$resource', '$location',
+    function($scope, $resource, $location){
+        $scope.save = function(){
+            var Videos = $resource('/api/videos');
+            Videos.save($scope.video, function(){
+                $location.path('/');
+            });
+        };
+    }]);
+
+app.controller('EditVideoCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function($scope, $resource, $location, $routeParams){	
+        var Videos = $resource('/api/videos/:id', { id: '@_id' }, {
+            update: { method: 'PUT' }
+        });
+
+        Videos.get({ id: $routeParams.id }, function(video){
+            $scope.video = video;
+        });
+
+        $scope.save = function(){
+            Videos.update($scope.video, function(){
+                $location.path('/');
+            });
+        }
     }]);
